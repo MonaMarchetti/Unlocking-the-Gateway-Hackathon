edd837bc070147804bbf1d52500c482a60ad6393
Gaelle Gaban <gaellegaban@gmail.com>
2019-03-31 05:39:54 +0200

    first integration on eu website

---
+++ Help and advice for EU nationals and their family - Your Europe_files/script.js
@@ -0,0 +1,16 @@
+function showValise() {
+  var x = document.getElementById("fonduTop");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+function closeValise() {
+    var x = document.getElementById("fonduTop");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
